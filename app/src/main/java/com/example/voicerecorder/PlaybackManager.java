e9b5d92542e788f147be64f3061b3cdd92348bba
DOAN MINH TAN <50938085+minhtandoan@users.noreply.github.com>
2020-11-28 20:21:43 +0700

    add RecordManager & PlaybackManager class
    Các hàm thực hiện ghi âm & phát âm

---
+++ app/src/main/java/com/example/voicerecorder/PlaybackManager.java
@@ -0,0 +1,89 @@
+package com.example.voicetest;
+
+import android.media.MediaPlayer;
+import android.media.PlaybackParams;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PlaybackManager {
+
+    private static MediaPlayer mMediaPlayer;
+    private static File file;
+    private static Integer pitch;
+
+    public static abstract class Callback {
+
+        /**
+         * Sau khi phát xong file
+         */
+        public void onCompletion() {
+        }
+    }
+
+    private Callback mCallback;
+
+    /**
+     * Cài đặt file input phát âm
+     *
+     * @param path Đường dẫn tuyệt đối
+     * @return true nếu file có tồn tại, false nếu file ko tồn tại
+     */
+    public boolean setOutputFile(String path) {
+        boolean result;
+        file = new File(path);
+        result = file.exists();
+        return result;
+    }
+
+    public void setPitch(int pitch) {
+        this.pitch = pitch;
+    }
+
+    public void startPlayback(Callback... callbacks) {
+        if (mMediaPlayer != null) {
+            return;
+        }
+        mCallback = (callbacks.length > 0) ? callbacks[0] : null;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private synchronized void start() throws IOException {
+        mMediaPlayer = new MediaPlayer();
+        if (mMediaPlayer == null) {
+            return;
+        }
+        mMediaPlayer.setDataSource(file.getAbsolutePath());
+
+        PlaybackParams playbackParams = new PlaybackParams();
+        if (pitch != null) {
+            playbackParams.setPitch(pitch);
+        }
+        mMediaPlayer.setPlaybackParams(playbackParams);
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if (mCallback != null) {
+                    mCallback.onCompletion();
+                }
+                mMediaPlayer.release();
+                mMediaPlayer = null;
+            }
+
+        });
+
+        mMediaPlayer.prepare();
+        mMediaPlayer.start();
+    }
+
+}
---
+++ app/src/main/java/com/example/voicerecorder/RecordManager.java
@@ -0,0 +1,72 @@
+package com.example.voicetest;
+
+import android.media.MediaRecorder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RecordManager {
+
+    private static MediaRecorder mMediaRecorder;
+    private static File file;
+
+    /**
+     * Tạo file output ghi âm
+     *
+     * @param path Đường dẫn tuyệt đối
+     * @return true nếu tạo file mới, false nếu file đã tồn tại
+     * @throws IOException
+     */
+    public boolean setOutputFile(String path) throws IOException {
+        boolean result;
+        file = new File(path);
+        result = file.createNewFile();
+        return result;
+    }
+
+    public void startRecord() {
+        if (mMediaRecorder != null) {
+            return;
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private synchronized void start() throws IOException {
+        mMediaRecorder = new MediaRecorder();
+        if (mMediaRecorder == null) {
+            return;
+        }
+        configMediaRecorder(mMediaRecorder);
+        mMediaRecorder.prepare();
+        mMediaRecorder.start();
+    }
+
+    private void configMediaRecorder(MediaRecorder mr) {
+        mr.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mr.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mr.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mr.setOutputFile(file.getAbsolutePath());
+    }
+
+    public void stopRecord() {
+        if (mMediaRecorder != null) {
+            stop();
+        }
+    }
+
+    private void stop() {
+        mMediaRecorder.stop();
+        mMediaRecorder.release();
+        mMediaRecorder = null;
+    }
+
+}
